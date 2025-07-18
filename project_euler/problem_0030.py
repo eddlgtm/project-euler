367ed8ae11c2c8d56f7a328a4e4c4ad7ad26f213
eddlgtm <edward@robopsychology.io>
2025-07-18 20:48:38 +0200

    Complete 30

---
+++ project_euler/problem_0030.py
@@ -0,0 +1,8 @@
+# https://projecteuler.net/problem=30
+
+def is_sum_of_power_of_digits(number: int, exponent: int = 5) -> bool:
+    return number == sum([pow(int(d), exponent) for d in str(number)])
+
+assert is_sum_of_power_of_digits(9474, 4) == True
+
+print(sum([x for x in range(10000000) if is_sum_of_power_of_digits(x) is True]))
